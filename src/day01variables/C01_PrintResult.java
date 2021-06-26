1ff4a5b638add6965161a5fdd864432b930d9679
fsen.52 <furkansen.52@gmail.com>
2021-06-26 22:13:01 +0200

    Java Course First Day

---
+++ src/day01variables/C01_PrintResult.java
@@ -0,0 +1,13 @@
+package day01variables;
+
+public class C01_PrintResult {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		
+		System.out.println("this is last call");
+
+	}
+
+}
---
+++ src/day01variables/C02_Scanner01.java
@@ -0,0 +1,18 @@
+package day01variables;
+
+import java.util.Scanner;
+
+public class C02_Scanner01 {
+	
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Please enter one User Name : ");
+
+		String userName = scan.nextLine();
+		
+		System.out.println("User name is " + userName);
+
+	}
+}
