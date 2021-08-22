e014e127e01e602289377a350377095af1d44c2f
furkan <furkansen.52@gmail.com>
2021-08-22 20:35:47 +0200

    Ubuntu üzerinden deneme

---
+++ src/example/Example01.java
@@ -0,0 +1,14 @@
+package example;
+
+import java.util.Scanner;
+
+public class Example01 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String name = scan.next();
+
+        System.out.println("girilen isim : " + name);
+    }
+}
