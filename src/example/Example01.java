b655eeed255f16815c5046f60920481c8c3ddbf5
fsen52 <60448067+fsen52@users.noreply.github.com>
2022-01-24 00:51:42 +0100

    Update Example01.java

--- src/example/Example01.java
+++ src/example/Example01.java
@@ -7,13 +7,6 @@ public class Example01 {
     public static void main(String[] args) {
 //        Scanner scan = new Scanner(System.in);
 //        System.out.println("Lütfen ekrana yazdırmak istediğiniz ismi giriniz ...");
-//        String name = scan.next();
-
-
-        String a = "furkan";
-        String b = new String("fasta");
-
-
-
+//        Stringhjygyuguygıygygygub
     }
 }
