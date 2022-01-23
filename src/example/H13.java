6df5b4bc84b58b9086d577490e2b233352f8a984
fsen52 <60448067+fsen52@users.noreply.github.com>
2022-01-24 00:51:57 +0100

    Update H13.java

--- src/example/H13.java
+++ src/example/H13.java
@@ -8,27 +8,7 @@ import java.util.Scanner;
 
 public class H13 {
 	public static void main(String[] args) {
-		try {
-			FileInputStream fis = new FileInputStream(
-					"C:\\Users\\Furkan\\git\\java2021SummerTr\\src\\day39_Exceptions\\Other File");
-		
-		try {
-
-//			for(int i=0; i<200; i++) {
-//				
-//				System.out.print((char)fis.read());
-//			}
-			
-		while (true) {
-			
-			System.out.println((char)(fis.read()));
-			
-		}
-
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		esercgfvygvytvyt
 		
 		
 		
